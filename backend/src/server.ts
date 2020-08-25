0fe80de0dd0efe2ae06d4c73ce2ad5e51c206291
Felipe Duque <felipeduque@Felipes-iMac-Pro.local>
2020-08-25 14:17:32 -0300

    fix: file structure

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,8 @@ import express, { response } from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
   return response.json({ message: "Hello, World!" });
 });
