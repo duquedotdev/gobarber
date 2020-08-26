cdac143ad86c7739828f9b2991b3321e19ac97a0
Felipe Duque <felipeduque@Felipes-iMac-Pro.local>
2020-08-26 09:46:03 -0300

    refactor: appointments moved to a repository

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,23 +1,18 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
-interface IAppointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
-
-const appointments: IAppointment[] = [];
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
-  const findAppointmentsInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
+
+  const findAppointmentsInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
   );
 
   if (findAppointmentsInSameDate) {
@@ -26,13 +21,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
-
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return response.status(200).json(appointment);
 });
