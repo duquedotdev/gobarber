4be98102af97fd48fb3e76c86b40b716393c2983
Felipe Duque <duquedotdev@MacBook-Pro-de-Felipe.local>
2020-08-29 19:08:16 -0300

    refactor: transform AppointmentsRepository

--- backend/src/repositories/AppointmentsRepository.ts
+++ backend/src/repositories/AppointmentsRepository.ts
@@ -1,33 +1,13 @@
-import { isEqual } from 'date-fns';
-import Appointments from '../model/Appointment';
+import { EntityRepository, Repository } from 'typeorm';
+import Appointment from '../model/Appointment';
 
-interface ICreateAppointmentDTO {
-  provider: string;
-  date: Date;
-}
-
-class AppointmentsRepository {
-  public appointments: Appointments[];
-
-  constructor() {
-    this.appointments = [];
-  }
-
-  public all(): Appointments[] {
-    return this.appointments;
-  }
-
-  public findByDate(date: Date): Appointments | null {
-    const findAppointments = this.appointments.find(appointment =>
-      isEqual(date, appointment.date),
-    );
-    return findAppointments || null;
-  }
-
-  public create({ provider, date }: ICreateAppointmentDTO): Appointments {
-    const appointment = new Appointments({ provider, date });
-    this.appointments.push(appointment);
-    return appointment;
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
+  public async findByDate(date: Date): Promise<Appointment | null> {
+    const findAppointment = await this.findOne({
+      where: { date },
+    });
+    return findAppointment || null;
   }
 }
 
