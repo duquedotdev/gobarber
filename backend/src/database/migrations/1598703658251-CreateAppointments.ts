3a3f520c561df9a0a741ae5a64b61f7bf912b79d
Felipe Duque <duquedotdev@MacBook-Pro-de-Felipe.local>
2020-08-30 16:15:57 -0300

    🛠!refactor: id type changed to uuid

--- backend/src/database/migrations/1598703658251-CreateAppointments.ts
+++ backend/src/database/migrations/1598703658251-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1598703658251
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
