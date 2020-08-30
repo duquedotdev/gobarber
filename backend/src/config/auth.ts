f447c0af80b6416458d879d6015f234f4cb6f4da
Felipe Duque <felipe@duque.dev>
2020-08-30 18:58:15 -0300

    📦feat: JWT authentication

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  jwt: {
+    secret:
+      'a4a7145c32886dbbc0fc6c3b553b2e1d12ebc84ced642bee09af9cb88044f7c339ba6342a2ecab0840c452f74ee63748d4300716bc6cead1b842c9bf56b55005',
+    expiresIn: '1d',
+  },
+};
