e4e9d3b6b6dd63603cfaaa41796a528b30289686
Felipe Duque <duquedotdev@MacBook-Pro-de-Felipe.local>
2020-08-30 16:45:22 -0300

    🛠refactor: change request body to providerId

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -14,7 +14,7 @@ appointmentsRouter.get('/', async (request, response) => {
 
 appointmentsRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { providerId, date } = request.body;
 
     const parsedDate = parseISO(date);
 
@@ -22,7 +22,7 @@ appointmentsRouter.post('/', async (request, response) => {
 
     const appointment = await createAppointment.execute({
       date: parsedDate,
-      provider,
+      providerId,
     });
 
     return response.status(200).json(appointment);
