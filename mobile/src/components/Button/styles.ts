16f7a324bb803b8b4ce54bc14d749736d4397937
Felipe Duque <felipe@duque.dev>
2020-09-09 14:44:33 -0300

    chore: onform set in SignUp and SignIn pages

--- mobile/src/components/Button/styles.ts
+++ mobile/src/components/Button/styles.ts
@@ -2,13 +2,13 @@ import styled from 'styled-components/native';
 import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled(RectButton)`
-  width: 100%;
   height: 60px;
   background: #ff9900;
   border-radius: 10px;
+  margin-top: 8px;
+
   justify-content: center;
   align-items: center;
-  margin-top: 8px;
 `;
 export const ButtonText = styled.Text`
   font-family: 'RobotoSlab-Medium';
--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import {
   Image,
   View,
@@ -8,6 +8,8 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 import logoImg from '../../assets/logo.png';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -22,6 +24,10 @@ import {
 
 const SignIn: React.FC = () => {
   const navigation = useNavigation();
+  const formRef = useRef<FormHandles>(null);
+  const handleSignIn = useCallback((data: object) => {
+    console.log(data);
+  }, []);
 
   return (
     <>
@@ -39,16 +45,17 @@ const SignIn: React.FC = () => {
             <View>
               <Title>Faça seu logon</Title>
             </View>
-
-            <Input name="email" icon="mail" placeholder="E-mail" />
-            <Input name="passwork" icon="lock" placeholder="Senha" />
-            <Button
-              onPress={() => {
-                console.log('Ok!');
-              }}
-            >
-              Entrar
-            </Button>
+            <Form ref={formRef} onSubmit={handleSignIn}>
+              <Input name="email" icon="mail" placeholder="E-mail" />
+              <Input name="password" icon="lock" placeholder="Senha" />
+              <Button
+                onPress={() => {
+                  formRef.current?.submitForm();
+                }}
+              >
+                Entrar
+              </Button>
+            </Form>
             <ForgotPassword
               onPress={() => {
                 console.log('Ok!');
